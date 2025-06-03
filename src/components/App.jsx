f4008d0b5d7b20469cf22fb970c402fc1f5a2860
Roxana Solano <roxansolan@gmail.com>
2025-06-03 09:55:01 +0200

    paint the table

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,32 @@
-import '../styles/App.scss';
+import "../styles/App.scss";
+import adabalersData from "../data/adabalers.json";
+import { useState } from "react";
 
 function App() {
+  const [girlsAdalabs] = useState(adabalersData);
+  /* console.log("aaaaaaaaaaa", adabalersData); */
   return (
-    <p>Escribe aquí tu código HTML</p>
+    <div>
+      <h1>Adalabers</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {girlsAdalabs.map((valueGirls, index) => (
+            <tr key={index}>
+              <td>{valueGirls.name}</td>
+              <td>{valueGirls.counselor}</td>
+              <td>{valueGirls.speciality}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
